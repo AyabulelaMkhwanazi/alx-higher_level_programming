b392af7932050c9492f20ca9580c9bb69c097e58
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-01-08 15:59:06 +0200

    Task complete

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(' '.join(str(num) for num in row))
+    for i in matrix:
+        print(' '.join('{:d}'.format(j)for j in i))
+
