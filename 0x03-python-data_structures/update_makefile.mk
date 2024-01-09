10b8d6879b879f4fd313c670562b8b642316c6f3
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-01-09 08:03:23 +0200

    12. Switch

--- 0x03-python-data_structures/update_makefile.mk
+++ 0x03-python-data_structures/update_makefile.mk
@@ -1,11 +1,19 @@
 TARGET_MAKEFILE := makefile
-
+OLD_MAIN_FILE := $(shell grep -oP 'SCRIPT = \K.*' $(TARGET_MAKEFILE))
 MAIN_FILE := $(shell ls -t *-main.py | head -n 1)
 TEST_FILE := $(shell ls -t *.py | head -n 1)
 
 .PHONY: update_makefile
 update_makefile:
 	@echo "Updating $(TARGET_MAKEFILE)..."
-	@sed -i 's|SCRIPT = .*|SCRIPT = $(MAIN_FILE)|' $(TARGET_MAKEFILE)
+	@if [ "$(MAIN_FILE)" = "$(OLD_MAIN_FILE)" ]; then \
+		echo "No new main file found..."; \
+		sed -i 's|SCRIPT = .*|SCRIPT = |' $(TARGET_MAKEFILE); \
+		sed -i 's|EXECUTABLE = .*|EXECUTABLE = $(TEST_FILE)|' $(TARGET_MAKEFILE); \
+		chmod u+x $(TEST_FILE); \
+	else \
+		sed -i 's|SCRIPT = .*|SCRIPT = $(MAIN_FILE)|' $(TARGET_MAKEFILE); \
+		sed -i 's|EXECUTABLE = .*|EXECUTABLE = $(MAIN_FILE)|' $(TARGET_MAKEFILE); \
+		chmod u+x $(MAIN_FILE); \
+	fi
 	@sed -i 's|FILE = .*|FILE = $(TEST_FILE)|' $(TARGET_MAKEFILE)
-
