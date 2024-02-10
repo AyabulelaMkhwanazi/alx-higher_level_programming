5d28c03312c1f6e9efd5d82a43f8a2968ed65980
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-02-10 17:50:31 +0200

    Errors

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -89,4 +89,5 @@ class Square:
         else:
             return ("\n" * self.__position[1] + (" "
                     * self.__position[0] + "#" *
-                    self.__size + "\n") * self.__size)
+                    self.__size + "\n") * (self.__size - 1) +
+                    " " * self.__position[0] + "#" * self.__size)
