a32e474f218562acf26704c54b43f516f693d660
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-02-14 15:26:07 +0200

    check

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -20,17 +20,11 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    if isinstance(text, (int, float)):
-        raise TypeError("text must be a string")
 
-    string = ""
-    for char in text:
-        string += char
-        if char in ['.', '?', ':']:
-            string += "\n\n"
-
-    lines = string.splitlines()
-    stripped_lines = [line.strip() for line in lines]
-
-    final_text = "\n".join(stripped_lines)
-    print(final_text)
+    for char in ".:?":
+        text = text.replace(char, char + "\n\n")
+    lines = text.split("\n")
+    for i in range(len(lines)):
+        lines[i] = lines[i].rstrip()  # Only remove trailing spaces
+    text = "\n".join(lines)
+    print(text)
