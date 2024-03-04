b2b9ebfa192dc039a1c52ea12485262e0cbbe7b5
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-03-04 20:03:45 +0200

    pycodestyle checks.

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,7 @@ This module contains a function that checks if the object is an instance of a
 class that inherited (directly or indirectly) from the specified class.
 """
 
+
 def inherits_from(obj, a_class):
     """Returns True if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class; otherwise False
