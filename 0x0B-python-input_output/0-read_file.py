c612e961518fb26981cd1cb67d37a76e6ae072c5
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-03-05 16:53:35 +0200

    removed the newline character.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     This module reads a file (UTF-8) and prints to stdout.
     """
     with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
