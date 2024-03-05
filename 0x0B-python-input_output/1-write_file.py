f5c7a35a303ac798281260f955641557b42b278f
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-03-05 17:13:10 +0200

    pycodestyle checks.

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -13,10 +13,9 @@ def write_file(filename="", text=""):
     Args:
         filename: the name of the file to write to.
         text: the text to write into the file.
-    
+
     Returns:
         the number of characters written.
     """
     with open(filename, mode="w", encoding="utf-8") as file:
         return file.write(text)
-    
