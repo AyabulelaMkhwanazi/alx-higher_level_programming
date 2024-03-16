940eec64ebef9bde4bbe087448bd87d4f91d1e6a
AyabulelaMkhwanazi <ayabulelamkhwanazi@gmail.com>
2024-03-16 16:04:30 +0200

    cities

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -9,7 +9,7 @@ USE hbtn_0d_usa;
 
 /* third: create the table in the created database */
 CREATE TABLE IF NOT EXISTS states (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY(id)
 );
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,8 +7,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
 /* third: create the table in the database */
-CREATE TABLE IF NOT EXISTS citites (
-	id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS cities (
+	id INT AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY(id),
